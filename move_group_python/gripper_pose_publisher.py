4f75bafb87ab369b99c847c3a9c65ca54b5161c1
unclezalman <160236291+unclezalman@users.noreply.github.com>
2024-05-30 11:53:40 +0100

    Update gripper_pose_publisher.py

--- move_group_python/gripper_pose_publisher.py
+++ move_group_python/gripper_pose_publisher.py
@@ -1,32 +1,3 @@
-'''
-#!/usr/bin/env python
-
-import rospy
-import geometry_msgs.msg
-
-def publisher():
-    rospy.init_node('target_pose_publisher', anonymous=True)
-    pub = rospy.Publisher('/target_pose', geometry_msgs.msg.Pose, queue_size=10)
-    rate = rospy.Rate(10)  # 10 Hz
-
-    while not rospy.is_shutdown():
-        pose = geometry_msgs.msg.Pose()
-        pose.position.x = 0.4  # Update with the desired position
-        pose.position.y = 0.1  # Update with the desired position
-        pose.position.z = 0.4  # Update with the desired position
-        pose.orientation.w = 1.0
-
-        rospy.loginfo(f"Publishing new target pose: {pose}")
-        pub.publish(pose)
-        rate.sleep()
-
-if __name__ == '__main__':
-    try:
-        publisher()
-    except rospy.ROSInterruptException:
-        pass
-'''
-
 #!/usr/bin/env python
 
 import rospy
@@ -44,6 +15,7 @@ def publisher():
         x = float(input("Enter x position: "))
         y = float(input("Enter y position: "))
         z = float(input("Enter z position: "))
+        # User can input 'open' or 'close'
         gripper = input("Enter gripper position: ").strip()
 
         # Create a new Pose message
@@ -53,7 +25,7 @@ def publisher():
         pose.position.z = z
         pose.orientation.w = 1.0  # Assuming the same orientation for simplicity
 
-        # Log and publish the pose
+        # Log and publish the pose and gripper position
         rospy.loginfo(f"Publishing new target pose: {pose}")
         pub.publish(pose)
         grip_pub.publish(gripper)
