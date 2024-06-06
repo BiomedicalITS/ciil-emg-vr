bb3bd48beb66b5d95e6d0eec918ab14f6255e1c2
Gabgagne <darkoumce.gabgagne@gmail.com>
2024-06-06 19:26:44 +0200

    Added a lot of stuff. Also fixed online datahandler in online main. I'm an idiot

--- nfc/globals.py
+++ nfc/globals.py
@@ -9,26 +9,25 @@ try:
 except ImportError:
     pass
 
-DEVICE = "bio"  # "emager", "myo" or "bio"
+DEVICE = "myo"  # "emager", "myo" or "bio"
+LIBEMG_GESTURE_IDS = [1, 2, 3, 8, 26, 30]
+# LIBEMG_GESTURE_IDS = [1, 2, 3, 4, 5, 14, 15]  # + abduction + adduction
 
-BASE_DIR = f"data/{DEVICE}/"
+PEUDO_LABELS_PORT = 5111
+PREDS_PORT = 5112
+PREDS_IP = "127.0.0.1"
+
+BASE_DIR = f"data_nfc/{DEVICE}/"
 TRAIN_DATA_DIR = BASE_DIR + "train/"
 FINETUNE_DATA_DIR = BASE_DIR + "finetune/"
 MODEL_PATH = BASE_DIR + "model.pth"
 
-# LIBEMG_GESTURE_IDS = [1, 2, 3, 4, 5] # minimal
-# LIBEMG_GESTURE_IDS = [1, 2, 3, 4, 5, 6, 7]  # + supi + prona
-LIBEMG_GESTURE_IDS = [1, 2, 3, 4, 5, 14, 15]  # + abduction + adduction
-# LIBEMG_GESTURE_IDS = [1, 2, 3, 4, 5, 6, 7, 14, 15]  # + supi + prona + abduction + adduction
-
-PEUDO_LABELS_PORT = 5111
-ROBOT_PORT = 5112
-ROBOT_IP = "192.168.50.39"  # nvidia
+LIBEMG_GESTURES_DIR = BASE_DIR.replace(DEVICE, "gestures")
 
 EMG_DATA_SHAPE = (4, 16) if DEVICE == "emager" else (1, 8)
 EMG_SAMPLING_RATE = 1000 if DEVICE == "emager" else 1500 if DEVICE == "bio" else 200
 EMG_SCALING_FACTOR = (
-    2.0**15 if DEVICE == "emager" else 0.008 if DEVICE == "bio" else 128.0
+    2.0**15 if DEVICE == "emager" else 0.001 if DEVICE == "bio" else 128.0
 )
 EMG_NOTCH_FREQ = 50
 EMG_WINDOW_MS = 25
